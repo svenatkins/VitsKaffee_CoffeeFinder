abb450068d0e89edfbdb8094532c23651621cde0
Anna Wagner <anna.wagner@maibornwolff.de>
2020-09-28 17:13:22 +0200

    add first setting in admin area (not doing anything yet)

--- vits_coffee_finder/admin.php
+++ vits_coffee_finder/admin.php
@@ -1,7 +1,26 @@
 <?php
-function vits_cf_admin_plugin(){
-    ?><h1>Coffee Finder</h1>
-    <p>manage hierarchic tags and other options here, it necessary</p>
+
+function vits_cf_question_field_callback_function() {
+    echo "<input type='checkbox' name='vits_cf_question_field' id='vits_cf_question_field' value='1'" .
+        checked(1, get_option('vits_cf_question_field'), false) .
+        "/>";
+}
+
+function vits_cf_question_section_callback_function() {
+    echo '<p>Fill in your questions and answers</p>';
+}
+
+function vits_cf_admin_plugin()
+{
+    ?>
+    <h1>Coffee Finder</h1>
+    <form method="POST" action="options.php">
+    <?php
+    settings_fields( 'question_group' );
+    do_settings_sections('reading');
+    submit_button();
+    ?>
+    </form>
     <?php
 }
 
@@ -14,6 +33,24 @@ function vits_cf_setup_admin_plugin()
             "manage_options",
             "coffee-finder-plugin",
             'vits_cf_admin_plugin',
-            'dashicons-products', 79);
+            'dashicons-products',
+            79);
+        add_settings_section(
+            'vits_cf_question_section',
+            'Manage Questions and Answers',
+            'vits_cf_question_section_callback_function',
+            'reading'
+        );
+        add_settings_field(
+            'vits_cf_question_field',
+            'Question 0',
+            'vits_cf_question_field_callback_function',
+            'reading',
+            'vits_cf_question_section',
+            [
+                'label_for' => 'vits_cf_question_field'
+            ]
+        );
+        register_setting('question_group', 'vits_cf_question_field');
     });
 }
